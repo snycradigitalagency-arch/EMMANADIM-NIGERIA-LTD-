7674e05c714c9f5c4f768263a8982a0bbe23012b
Syncra Digital Agency <syncradigitalagency@gmail.com>
2026-07-06 12:48:06 -0700

    Fix alias resolution directly in webpack config

--- emmanadim-app/next.config.js
+++ emmanadim-app/next.config.js
@@ -1,9 +1,15 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.supabase.co" },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
   },
 };
 
